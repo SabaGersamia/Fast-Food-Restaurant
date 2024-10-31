99b27ee00cfc582c19b0b84ae1926391ee6beb5d
Saba <gersamia.saba25@gmail.com>
2024-10-31 15:35:00 +0400

    init

---
+++ Burger Bar/User/User.Master.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Burger_Bar.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("HomePage.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                // add the control to the panel
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
